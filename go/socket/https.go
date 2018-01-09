79077b116bce0acc1a7eedb28c2cacf1e87fb92a
chang-ning <spiderpower02@gmail.com>
2018-01-09 01:29:04 -0800

    Fix the error of example in https.go

--- go/socket/https.go
+++ go/socket/https.go
@@ -12,7 +12,7 @@ const (
 )
 
 // usage:
-//	$ openssl genrsa -out server.key 2048
+//	$ openssl genrsa -out key.pem 2048
 //	$ openssl req -new -x509 -sha256 -key key.pem -out cert.pem -days 365 \
 //		-subj "/C=TW/ST=Taiwan/L=Taipei/O=OrgName/OU=OrgUnitName/CN=example.com/"
 // 	$ ./https &
